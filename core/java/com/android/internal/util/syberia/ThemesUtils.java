CherishOS/android_frameworks_base

SystemUI: Introduce Custom Statusbar Signal & Wi-Fi icons [1/3]
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

core/java/com/android/internal/util/cherish/ThemesUtils.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.cherish;
+
+import static android.os.UserHandle.USER_SYSTEM;
+
+import android.app.UiModeManager;
+import android.content.Context;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.provider.Settings;
+import android.util.Log;
+
+public class ThemesUtils {
+
+    public static final String TAG = "ThemesUtils";
+
+    // Statusbar Signal icons
+    private static final String[] SIGNAL_BAR = {
+            "com.custom.systemui.signalbar_a",
+            "com.custom.systemui.signalbar_b",
+            "com.custom.systemui.signalbar_c",
+    };
+
+    // Statusbar Wifi icons
+    private static final String[] WIFI_BAR = {
+            "com.custom.systemui.wifibar_a",
+            "com.custom.systemui.wifibar_b",
+            "com.custom.systemui.wifibar_c",
+    };
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -69,6 +69,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.om.IOverlayManager;
 import android.content.pm.IPackageManager;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -272,6 +273,8 @@ import com.android.wm.shell.bubbles.Bubbles;
 import com.android.wm.shell.legacysplitscreen.LegacySplitScreen;
 import com.android.wm.shell.startingsurface.SplashscreenContentDrawer;
 import com.android.wm.shell.startingsurface.StartingSurface;
+
+import com.android.internal.util.cherish.ThemesUtils;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -707,6 +710,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     private final NetworkController mNetworkController;
     private final LifecycleRegistry mLifecycle = new LifecycleRegistry(this);
     protected final BatteryController mBatteryController;
+    private IOverlayManager mOverlayManager;
     private CurrentUserTracker mUserTracker;
     protected boolean mPanelExpanded;
     private UiModeManager mUiModeManager;
@@ -973,6 +977,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     public void start() {
         mScreenLifecycle.addObserver(mScreenObserver);
         mWakefulnessLifecycle.addObserver(mWakefulnessObserver);
+        mOverlayManager = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
         mUiModeManager = mContext.getSystemService(UiModeManager.class);
         mBypassHeadsUpNotifier.setUp();
         if (mBubblesOptional.isPresent()) {

SOVIET-ANDROID/platform_frameworks_base

core/java/com/android/internal/util/syberia/ThemesUtils.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.syberia;
+
+import static android.os.UserHandle.USER_SYSTEM;
+
+import android.app.UiModeManager;
+import android.content.Context;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.provider.Settings;
+import android.util.Log;
+
+public class ThemesUtils {
+
+    public static final String TAG = "ThemesUtils";
+
+    // Statusbar Signal icons
+    private static final String[] SIGNAL_BAR = {
+            "com.custom.systemui.signalbar_a",
+            "com.custom.systemui.signalbar_b",
+            "com.custom.systemui.signalbar_c",
+    };
+
+    // Statusbar Wifi icons
+    private static final String[] WIFI_BAR = {
+            "com.custom.systemui.wifibar_a",
+            "com.custom.systemui.wifibar_b",
+            "com.custom.systemui.wifibar_c",
+    };
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -65,6 +65,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.om.IOverlayManager;
 import android.content.pm.IPackageManager;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -260,6 +261,8 @@ import com.android.wm.shell.bubbles.Bubbles;
 import com.android.wm.shell.legacysplitscreen.LegacySplitScreen;
 import com.android.wm.shell.startingsurface.SplashscreenContentDrawer;
 import com.android.wm.shell.startingsurface.StartingSurface;
+
+import com.android.internal.util.syberia.ThemesUtils;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -698,6 +701,7 @@ public class StatusBar extends SystemUI implements
     private final NetworkController mNetworkController;
     private final LifecycleRegistry mLifecycle = new LifecycleRegistry(this);
     protected final BatteryController mBatteryController;
+    private IOverlayManager mOverlayManager;
     protected boolean mPanelExpanded;
     private UiModeManager mUiModeManager;
     protected boolean mIsKeyguard;
@@ -975,6 +979,8 @@ public class StatusBar extends SystemUI implements
     public void start() {
         mScreenLifecycle.addObserver(mScreenObserver);
         mWakefulnessLifecycle.addObserver(mWakefulnessObserver);
+        mOverlayManager = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
         mUiModeManager = mContext.getSystemService(UiModeManager.class);
         mBypassHeadsUpNotifier.setUp();
         if (mBubblesOptional.isPresent()) {